bc98dac077929447c9d96ca13accef94a941fb15
KYEONGMIN CHO <ggingmin@gmail.com>
2023-09-21 14:12:23 +0900

    first commit

---
+++ demo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-3&h$&-v2l1=n0(#v%jdzew4i@s8^*^01aya3u6t&%v65tre)t3"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
